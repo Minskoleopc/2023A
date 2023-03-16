3f3592f72c2a5ea6f5c85fe72e7259793544175b
Minskoleopc <minskoleopc@gmail.com>
2023-03-16 20:07:24 +0530

    added

--- DOM17/script.js
+++ DOM17/script.js
@@ -25,21 +25,23 @@ console.log(headOne)
 //     window.location.reload()
 // })
 
-let liList = document.querySelectorAll('li')
-let ulList = document.querySelector('ul')
+// let liList = document.querySelectorAll('li')
+// let ulList = document.querySelector('ul')
 
-// event bubbling
-ulList.addEventListener('mouseover',function(){
-    for(let i = 0 ; i < liList.length ; i++){
-        liList[i].textContent =  liList[i].textContent.toUpperCase()
-    }
-})
+// // event bubbling
+// ulList.addEventListener('mouseover',function(){
+//     for(let i = 0 ; i < liList.length ; i++){
+//         liList[i].textContent =  liList[i].textContent.toUpperCase()
+//     }
+// })
 
-ulList.addEventListener('mouseout',function(){
-    for(let i = 0 ; i < liList.length ; i++){
-        liList[i].textContent =  liList[i].textContent.toLowerCase()
-    }
-})
+// ulList.addEventListener('mouseout',function(){
+//     for(let i = 0 ; i < liList.length ; i++){
+//         liList[i].textContent =  liList[i].textContent.toLowerCase()
+//     }
+// })
+
+//
 
 
 
